a50e9890e138ec4160bacc7b9278e9645613e712
vr7517 <vivekrajput1924345@gmail.com>
2026-07-05 20:38:38 +0530

    PWA notification system update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Simple offline cache for Cadence.
 // Bump CACHE_VERSION whenever you change index.html to force an update.
-const CACHE_VERSION = "cadence-v2";
+const CACHE_VERSION = "cadence-v3";
 const CORE = [
   "./",
   "./index.html",
